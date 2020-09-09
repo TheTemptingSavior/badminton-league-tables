9e9783e519c858952643a6cad6b10a2ce28de9c1
Ethan Cotterell <ethancotterell@gmail.com>
2020-09-09 14:39:57 +0100

    Created models for table data

--- src/routes/web.php
+++ src/routes/web.php
@@ -16,25 +16,3 @@ $router->get('/', function () use ($router) {
 });
 
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where all the routes relating to the API are stored
-|
-*/
-
-$router->group(['prefix' => 'api'], function() use ($router) {
-    $router->get('/', function() use ($router) {
-        return Array("league" => LEAGUE_NAME);
-    });
-
-    $router->group(['prefix' => 'users'], function() use ($router) {
-        $router->get('/', ['uses' => 'UserController@listUsers']);
-        // These two are untested
-        $router->post('/', ['uses' => 'UserController@createUser']);
-        $router->delete('/{id}', ['uses' => 'UserController@deleteUser']);
-    });
-
-});
