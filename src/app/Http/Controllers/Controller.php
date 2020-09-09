97134efbba815770bf6504ed0ffef88ec693ed50
Ethan Cotterell <ethancotterell@gmail.com>
2020-09-09 15:39:04 +0100

    Implemented JWT authentication

--- src/app/Http/Controllers/Controller.php
+++ src/app/Http/Controllers/Controller.php
@@ -6,5 +6,18 @@ use Laravel\Lumen\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
-    //
+    /**
+     * Returns a JSON Web Token to the requester
+     * @param $token
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function respondWithToken($token)
+    {
+        // TODO: Figure out a way to state when the token will expire
+        return response()->json([
+            'token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => 3600
+        ], 200);
+    }
 }
