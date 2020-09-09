82a929803ffee055bc8a163581259232febdccca
Ethan Cotterell <ethancotterell@gmail.com>
2020-09-09 10:39:14 +0100

    Added doctrine/dbal for database modifications

--- src/app/Http/Controllers/UserController.php
+++ src/app/Http/Controllers/UserController.php
@@ -7,33 +7,46 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    /*
-    * List all users that have an account with the site
-    */
+    /**
+     * List all users that have an account with the site
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function listUsers()
     {
         return response()->json(User::all());
     }
 
-    /*
-     * Create a new user. This route requires the following data:
-     *  - username
-     *  - password
-     *  - admin
+    /**
+     * Create a new user. It should be noted that any newly created user
+     * is NOT an admin. For a user to become an admin, the site owner must
+     * change their admin status manually. This route requires the following
+     * data: 'username' and 'password'
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
      */
     public function createUser(Request $request)
     {
+        $this->validate(
+            $request,
+            [
+                'username' => 'required|unique:users',
+                'password' => 'required',
+            ]
+        );
         $newUser = User::create($request->all());
 
         return response()->json($newUser, 201);
     }
 
-    /*
-     * Delete a user with the given ID
+    /**
+     * Delete a user with the given ID. This action can only be performed by
+     * an admin. Returns no response on a successful delete
+     * @param string $id
+     * @return \Illuminate\Http\JsonResponse
      */
-    public function deleteUser(integer $id)
+    public function deleteUser(string $id)
     {
         User::findOrFail($id)->delete();
-        return response('Deleted Successfully', 204);
+        return response()->json([], 204);
     }
 }
