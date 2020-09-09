df4121e6dc2aaac5a21b115d09ffb90ae7a24021
ethan <ethan@example.com>
2020-09-09 15:11:41 +0100

    Created basic seeds for all present tables

---
+++ src/database/seeds/SeasonSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class SeasonSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('seasons')->insert([
+            "id" => 1,
+            'start' => strtotime('2017-09-01'),
+            'end' => strtotime('2018-08-31'),
+            'slug' => '17-18'
+        ]);
+        DB::table('seasons')->insert([
+            "id" => 2,
+            'start' => strtotime('2018-09-01'),
+            'end' => strtotime('2019-08-31'),
+            'slug' => '18-19'
+        ]);
+    }
+}
