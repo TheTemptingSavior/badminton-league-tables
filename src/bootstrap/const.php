9f64b98c1511a1bc0ccb40d18e981ff84b0088e9
Ethan Cotterell <ethancotterell@gmail.com>
2020-09-13 18:39:42 +0100

    Added swagger UI to the API

--- src/bootstrap/const.php
+++ src/bootstrap/const.php
@@ -1,7 +1,7 @@
 <?php
 
 defined('LEAGUE_NAME')      or define('LEAGUE_NAME',    'Rutland/Stamford');
-
+defined('API_HOST')         or define('API_HOST',       'localhost');
 // Admin user to be created on startup
 defined('ADMIN_USERNAME')   or define('ADMIN_USERNAME', 'admin');
 defined('ADMIN_PASSWORD')   or define('ADMIN_PASSWORD', 'helloworld');
