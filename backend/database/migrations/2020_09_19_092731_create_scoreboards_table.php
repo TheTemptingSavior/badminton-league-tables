9bd04d29933b82722b5b1b0577766e5918b9b43d
Ethan Cotterell <ethancotterell@gmail.com>
2020-09-19 20:05:37 +0100

    Created models for scoreboard and initial seed for scorecards in season 17-18

--- backend/database/migrations/2020_09_19_092731_create_scoreboards_table.php
+++ backend/database/migrations/2020_09_19_092731_create_scoreboards_table.php
@@ -14,7 +14,15 @@ class CreateScoreboardsTable extends Migration
     public function up()
     {
         Schema::create('scoreboards', function (Blueprint $table) {
-            $table->id();
+            $table->increments('id');
+            $table->foreignId('team')->constrained('teams');
+            $table->foreignId('season')->constrained('seasons');
+            $table->integer('played')->default(0);
+            $table->integer('points')->default(0);
+            $table->integer('wins')->default(0);
+            $table->integer('losses')->default(0);
+            $table->integer('for')->default(0);
+            $table->integer('against')->default(0);
             $table->timestamps();
         });
     }
