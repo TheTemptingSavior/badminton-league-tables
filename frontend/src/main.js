87e7718c2eead16bc260d2d9f1af48f7ad486f46
Ethan Cotterell <ethancotterell@gmail.com>
2020-09-20 23:48:25 +0100

    Added page and calculations for showing the current scoreboard

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,12 +1,17 @@
 import Vue from 'vue'
+import 'materialize-css/dist/css/materialize.min.css'
+import 'material-design-icons/iconfont/material-icons.css'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
 import App from './App.vue'
 import router from './router'
 import store from './store'
 
-import 'materialize-css/dist/css/materialize.min.css'
-import 'material-design-icons/iconfont/material-icons.css'
 
 Vue.config.productionTip = false
+Vue.use(VueAxios, axios);
+Vue.axios.defaults.baseURL = 'http://localhost';
 
 new Vue({
   router,
