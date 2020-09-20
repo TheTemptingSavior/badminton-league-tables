968bb11b60ab456073ad5d6637f44b4ca636fc1c
Ethan Cotterell <ethancotterell@gmail.com>
2020-09-20 21:51:52 +0100

    Updated docker-compose to fix queuing. Added job to run in queue on scorecard create

--- backend/app/Http/Controllers/ScoreboardController.php
+++ backend/app/Http/Controllers/ScoreboardController.php
@@ -3,10 +3,6 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Season;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Http\Request;
-use App\Models\User;
 use Illuminate\Support\Facades\DB;
 
 class ScoreboardController extends Controller
