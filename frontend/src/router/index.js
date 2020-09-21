840073f82c2159000eae979fb4f79282f425b100
Ethan Cotterell <ethancotterell@gmail.com>
2020-09-21 16:02:34 +0100

    Moved services under single domain (localhost)

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -10,22 +10,38 @@ const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home
+    component: Home,
+    meta: {
+      title: 'League Tables'
+    }
   },
   {
     path: '/login',
     name: 'Login',
-    component: Login
+    component: Login,
+    meta: {
+      title: 'Login'
+    }
   },
   {
     path: '/scoreboards',
     name: 'Scoreboards',
-    component: Scoreboards
+    component: Scoreboards,
+    meta: {
+      title: 'Scoreboards'
+    }
   }
 ]
 
 const router = new VueRouter({
   routes
 })
+router.afterEach((to) => {
+  // Use next tick to handle router history correctly
+  // see: https://github.com/vuejs/vue-router/issues/914#issuecomment-384477609
+  Vue.nextTick(() => {
+    document.title = to.meta.title || 'League Tables';
+  });
+});
 
 export default router
