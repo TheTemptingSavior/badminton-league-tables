5483b7b201a86d5e611c286ffeddd836ab72e83e
Ethan Cotterell <ethancotterell@gmail.com>
2020-09-21 17:22:26 +0100

    Restructured scoreboard page

---
+++ frontend/src/components/ScoreboardTable.vue
@@ -0,0 +1,40 @@
+<template>
+  <table class="striped responsive-table">
+    <thead>
+    <tr>
+      <th>Team</th>
+      <th>Played</th>
+      <th>Points</th>
+      <th>Wins</th>
+      <th>Losses</th>
+      <th>For</th>
+      <th>Against</th>
+    </tr>
+    </thead>
+    <tbody>
+    <tr v-for="row in data" :key="row.id">
+      <td>{{ row.name }}</td>
+      <td>{{ row.played }}</td>
+      <td>{{ row.points }}</td>
+      <td>{{ row.wins }}</td>
+      <td>{{ row.losses }}</td>
+      <td>{{ row.for }}</td>
+      <td>{{ row.against }}</td>
+    </tr>
+    </tbody>
+  </table>
+</template>
+
+<script>
+export default {
+  name: "ScoreboardTable",
+  props: {
+    data: Array,
+    error: String
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ frontend/src/components/SeasonModal.vue
@@ -0,0 +1,34 @@
+<template>
+  <div :id="id" class="modal">
+    <div class="modal-content">
+      <h4>Available seasons</h4>
+      <ul id="seasons" class="collection">
+        <li v-for="season in seasons" :key="season.id">{{ season.slug }}</li>
+      </ul>
+    </div>
+    <div class="modal-footer">
+      <a class="modal-close waves-effect waves-green btn-flat">Agree</a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SeasonModal",
+  props: {
+    id: String
+  },
+  data() {
+    return {
+      seasons: this.$store.state.seasons
+    }
+  },
+  created() {
+    this.$store.dispatch('loadSeasons');
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
