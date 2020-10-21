fe720a3c2435d2b3d918f9d02c26e89d34fb6e5f
ethan <ethan@example.com>
2020-10-21 11:15:28 +0100

    Added artisan commands to run updates for scoreboards

--- backend/app/Console/Kernel.php
+++ backend/app/Console/Kernel.php
@@ -13,7 +13,8 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        //
+        \App\Console\Commands\UpdateScoreboards::class,
+        \App\Console\Commands\UpdateScoreboard::class,
     ];
 
     /**
