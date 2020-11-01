2535106f081c10de36ff5e3d8ac67b84d0f8e5d9
Ethan Cotterell <ethancotterell@gmail.com>
2020-11-01 23:05:25 +0000

    Fixed navbar to use Vuetify

--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -1,61 +1,101 @@
 <template>
-  <div class="nav">
-    <nav class="light-blue lighten-1">
-      <div class="nav-wrapper">
-        <router-link to="/" class="brand-logo">League Tables</router-link>
-        <a href="#" data-target="mobile-demo" class="sidenav-trigger">
-          <i class="material-icons">menu</i>
-        </a>
-        <ul class="right hide-on-med-and-down">
-          <li v-bind:class="{ active: isHome }"><router-link to="/">Home</router-link></li>
-          <li v-bind:class="{ active: isScoreboards }"><router-link to="/scoreboards">Scoreboards</router-link></li>
-          <li v-bind:class="{ active: isTracker }"><router-link to="/tracker">Tracker</router-link></li>
+  <v-list nav dense>
+    <div v-if="!$vuetify.breakpoint.mdAndUp"><br /><br /></div>
+    <v-list-item-group v-model="group" active-class="orange--text text--accent-4">
+      <v-list-item to="/">
+          <v-list-item-icon>
+            <v-icon>mdi-home</v-icon>
+          </v-list-item-icon>
+          <v-list-item-title>Home</v-list-item-title>
+      </v-list-item>
+      <v-list-item to="/scoreboards">
+        <v-list-item-icon>
+          <v-icon>mdi-home</v-icon>
+        </v-list-item-icon>
+        <v-list-item-title>Scorecards</v-list-item-title>
+      </v-list-item>
+      <v-list-item to="/tracker">
+        <v-list-item-icon>
+          <v-icon>mdi-home</v-icon>
+        </v-list-item-icon>
+        <v-list-item-title>Tracker</v-list-item-title>
+      </v-list-item>
+      <v-list-item v-on:click="logout" v-if="isLoggedIn">
+        <v-list-item-icon>
+          <v-icon>mdi-home</v-icon>
+        </v-list-item-icon>
+        <v-list-item-title>Logout</v-list-item-title>
+      </v-list-item>
+      <v-list-item to="/admin" v-if="isLoggedIn">
+        <v-list-item-icon>
+          <v-icon>mdi-home</v-icon>
+        </v-list-item-icon>
+        <v-list-item-title>Admin</v-list-item-title>
+      </v-list-item>
+      <v-list-item to="/login" v-if="!isLoggedIn">
+        <v-list-item-icon>
+          <v-icon>mdi-home</v-icon>
+        </v-list-item-icon>
+        <v-list-item-title>Login</v-list-item-title>
+      </v-list-item>
+    </v-list-item-group>
 
-          <li v-if="isLoggedIn" v-bind:class="{ adminActive: isAdmin }"><router-link to="/admin">Admin</router-link></li>
-          <li v-if="!isLoggedIn" v-bind:class="{ active: isLogin }"><router-link to="/login">Login</router-link></li>
-          <li v-else v-on:click="logout"><router-link to="/">Logout</router-link></li>
-        </ul>
-      </div>
-    </nav>
+    <br />
+    <v-divider />
+    <br />
 
-    <ul class="sidenav" id="mobile-demo">
-      <li v-bind:class="{ active: isHome }"><router-link to="/">Home</router-link></li>
-      <li v-bind:class="{ active: isScoreboards }"><router-link to="/scoreboards">Scoreboards</router-link></li>
-      <li v-bind:class="{ active: isTracker }"><router-link to="/tracker">Tracker</router-link></li>
-
-      <li v-if="isLoggedIn" v-bind:class="{ active: isAdmin }"><router-link to="/admin">Admin</router-link></li>
-
-      <li v-if="!isLoggedIn" v-bind:class="{ active: isLogin }"><router-link to="/login">Login</router-link></li>
-      <li v-else v-on:click="logout"><router-link to="/">Logout</router-link></li>
-    </ul>
-
-    <AdminNavBar v-if="isLoggedIn" />
-  </div>
+    <v-list-item-group v-model="adminGroup" v-if="isLoggedIn" active-class="orange--text text--accent-4">
+      <v-list-item to="/admin/scorecards/new">
+        <v-list-item-icon>
+          <v-icon>mdi-home</v-icon>
+        </v-list-item-icon>
+        <v-list-item-title>New Scorecard</v-list-item-title>
+      </v-list-item>
+      <v-list-item to="/admin/scorecards/manage">
+        <v-list-item-icon>
+          <v-icon>mdi-home</v-icon>
+        </v-list-item-icon>
+        <v-list-item-title>Manage Scorecards</v-list-item-title>
+      </v-list-item>
+      <v-list-item to="/admin/teams">
+        <v-list-item-icon>
+          <v-icon>mdi-home</v-icon>
+        </v-list-item-icon>
+        <v-list-item-title>Teams</v-list-item-title>
+      </v-list-item>
+      <v-list-item to="/admin/analytics">
+        <v-list-item-icon>
+          <v-icon>mdi-home</v-icon>
+        </v-list-item-icon>
+        <v-list-item-title>Analytics</v-list-item-title>
+      </v-list-item>
+      <v-list-item to="/admin/users">
+        <v-list-item-icon>
+          <v-icon>mdi-home</v-icon>
+        </v-list-item-icon>
+        <v-list-item-title>Users</v-list-item-title>
+      </v-list-item>
+      <v-list-item to="/admin/help">
+        <v-list-item-icon>
+          <v-icon>mdi-home</v-icon>
+        </v-list-item-icon>
+        <v-list-item-title>Help</v-list-item-title>
+      </v-list-item>
+    </v-list-item-group>
+  </v-list>
 </template>
 
 <script>
-import AdminNavBar from "@/components/admin/AdminNavBar";
 export default {
   name: "NavBar",
-  components: {AdminNavBar},
+  components: {},
   data() {
     return {
-      dropdownName: "admin-dropdown"
+      group: null,
+      adminGroup: null,
     }
   },
   computed: {
-    isHome() {
-      return this.$route.name === 'Home';
-    },
-    isScoreboards() {
-      return this.$route.name === 'Scoreboards';
-    },
-    isTracker() {
-      return this.$route.name === 'Tracker';
-    },
-    isLogin() {
-      return this.$route.name === 'Login';
-    },
     isAdmin() {
       return this.$route.name.toLowerCase().startsWith('admin');
     },
@@ -77,7 +117,7 @@ export default {
       // TODO: Redirect to the home page
       console.log("User logging out");
       this.$store.dispatch('logoutUser');
-    }
+    },
   },
   created() {
     this.unwatch = this.$store.watch(
@@ -105,7 +145,5 @@ export default {
 </script>
 
 <style scoped>
-.adminActive {
-  background: #039be5;
-}
+
 </style>
