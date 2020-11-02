e55304f5ab05992045bef9e9d7d108535755f1c6
Ethan Cotterell <ethancotterell@gmail.com>
2020-11-02 17:40:48 +0000

    Completed the new scorecard page to use Vuetify

--- frontend/src/views/admin/AdminHome.vue
+++ frontend/src/views/admin/AdminHome.vue
@@ -1,18 +1,20 @@
 <template>
-  <div id="admin-home">
-    <div class="container">
-      <h1>Logged in successfully</h1>
-      <hr />
-      <p>
-        Maybe some stats about the site??
-      </p>
-      <ul>
-        <li>Number of teams</li>
-        <li>Number of seasons</li>
-        <li>Number of games played (total + this season)</li>
-      </ul>
-    </div>
-  </div>
+  <v-container>
+    <v-row>
+      <v-col cols="12" xs="12" class="text-center">
+        <h3 class="accent--text text-h3">Logged in successfully</h3>
+      </v-col>
+    </v-row>
+    <v-divider />
+    <p>
+      Maybe some stats about the site??
+    </p>
+    <ul>
+      <li>Number of teams</li>
+      <li>Number of seasons</li>
+      <li>Number of games played (total + this season)</li>
+    </ul>
+  </v-container>
 </template>
 
 <script>
