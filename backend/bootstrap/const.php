736a0c0d605e8d0ee230d9be7faa14adc3de11a8
Ethan Cotterell <ethancotterell@gmail.com>
2021-07-20 21:41:48 +0100

    constants: fix issues with definitions

--- backend/bootstrap/const.php
+++ backend/bootstrap/const.php
@@ -11,13 +11,13 @@
 */
 
 // League specific constants
-defined('LEAGUE_NAME')      or define('LEAGUE_NAME',        getenv('LEAGUE_NAME') ?? 'Rutland/Stamford');
-defined('LEAGUE_CODE')      or define('LEAGUE_CODE',        getenv('LEAGUE_CODE') ?? 'rs');
-defined('API_HOST')         or define('API_HOST',           getenv('API_HOST') ?? 'localhost');
+defined('LEAGUE_NAME')      or define('LEAGUE_NAME',        getenv('LEAGUE_NAME') ? getenv('LEAGUE_NAME') : 'Rutland/Stamford');
+defined('LEAGUE_CODE')      or define('LEAGUE_CODE',        getenv('LEAGUE_CODE') ? getenv('LEAGUE_CODE') : 'rs');
+defined('API_HOST')         or define('API_HOST',           getenv('API_HOST') ? getenv('API_HOST') : 'localhost');
 
 // Admin user to be created on startup
-defined('ADMIN_USERNAME')   or define('ADMIN_USERNAME',     getenv('ADMIN_USERNAME') ?? 'admin');
-defined('ADMIN_PASSWORD')   or define('ADMIN_PASSWORD',     getenv('ADMIN_PASSWORD') ?? 'helloworld');
+defined('ADMIN_USERNAME')   or define('ADMIN_USERNAME',     getenv('ADMIN_USERNAME') ? getenv('ADMIN_USERNAME') : 'admin');
+defined('ADMIN_PASSWORD')   or define('ADMIN_PASSWORD',     getenv('ADMIN_PASSWORD') ? getenv('ADMIN_PASSWORD)') : 'helloworld');
 
 // Application constants
 defined('TOKEN_EXPIRATION') or define('TOKEN_EXPIRATION',   3600);
