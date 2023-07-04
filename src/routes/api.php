ceb3d35ac53787afd306958a2d9693148601769d
TheTemptingSavior <TheTemptingSavior@protonmail.com>
2023-07-04 11:47:31 +0100

    Implemented JWT authentication

--- src/routes/api.php
+++ src/routes/api.php
@@ -14,14 +14,23 @@ $router->group(['prefix' => 'api'], function() use ($router) {
         return Array("league" => LEAGUE_NAME);
     });
 
-    $router->group(['prefix' => 'users'], function() use ($router) {
+    $router->group(['prefix' => 'auth'], function() use ($router) {
+        $router->post('/login', ['as' => 'login', 'uses' => 'AuthController@login']);
+        $router->get('/logout', ['as' => 'logout', 'uses' => 'AuthController@logout']);
+    });
+
+    $router->group(['prefix' => 'auth'], function() use ($router) {
+        $router->get('/me', ['as' => 'me', 'uses' => 'AuthController@me']);
+    });
+
+    $router->group(['prefix' => 'users', 'middleware' => 'auth'], function() use ($router) {
         // Get a list of users
-        $router->get('/', ['uses' => 'UserController@listUsers']);
+        $router->get('/', ['as' => 'user-list', 'uses' => 'UserController@listUsers']);
         // Create a new user
-        $router->post('/', ['uses' => 'UserController@createUser']);
+        $router->post('/', ['as' => 'user-create', 'uses' => 'UserController@createUser']);
         // Delete an existing user
-        $router->delete('/{id}', ['uses' => 'UserController@deleteUser']);
+        $router->delete('/{id}', ['as' => 'user-detail', 'uses' => 'UserController@deleteUser']);
         // Update an existing user
-        $router->put('/{id}', ['uses' => 'UserController@updateUser']);
+        $router->put('/{id}', ['as' => 'user-delete', 'uses' => 'UserController@updateUser']);
     });
 });
