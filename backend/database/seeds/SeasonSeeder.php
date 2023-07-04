4cce970c2f849eb07873c36ffadbba41b379ee0c
TheTemptingSavior <TheTemptingSavior@protonmail.com>
2023-07-04 11:47:33 +0100

    Updated db indices to support other databases. Moved development to a MySQL database

--- backend/database/seeds/SeasonSeeder.php
+++ backend/database/seeds/SeasonSeeder.php
@@ -1,7 +1,7 @@
 <?php
 
+use App\Models\Season;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\DB;
 
 class SeasonSeeder extends Seeder
 {
@@ -12,17 +12,18 @@ class SeasonSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('seasons')->insert([
-            "id" => 1,
-            'start' => strtotime('2017-09-01'),
-            'end' => strtotime('2018-08-31'),
-            'slug' => '17-18',
-        ]);
-        DB::table('seasons')->insert([
-            "id" => 2,
-            'start' => strtotime('2018-09-01'),
-            'end' => strtotime('2019-08-31'),
-            'slug' => '18-19',
-        ]);
+        $s1 = new Season;
+        $s1->id = 1;
+        $s1->start = '2017-09-01';
+        $s1->end = '2018-08-31';
+        $s1->slug = '17-18';
+        $s1->save();
+
+        $s2 = new Season;
+        $s2->id = 2;
+        $s2->start = '2018-09-01';
+        $s2->end = '2019-08-31';
+        $s2->slug = '18-19';
+        $s2->save();
     }
 }
