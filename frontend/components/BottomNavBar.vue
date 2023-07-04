4364702f933b6b925af1cb772d1b9faad80c8f39
TheTemptingSavior <TheTemptingSavior@protonmail.com>
2023-07-04 11:47:34 +0100

    Beginnings of a frontend

---
+++ frontend/components/BottomNavBar.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-footer app color="indigo">
+      <span class="white--text">
+        &copy; {{ new Date().getFullYear() }}
+      </span>
+  </v-footer>
+</template>
+
+<script>
+export default {
+  name: 'BottomNavBar',
+  props: ['drawer']
+}
+</script>
+
+<style scoped>
+
+</style>
