e53c60b64a6fd70800abc7f009b946737b6c6a4d
TheTemptingSavior <TheTemptingSavior@protonmail.com>
2023-07-04 11:54:25 +0100

    fix frontend api url

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -4,7 +4,7 @@ module.exports = {
         config.plugin('define')
             .tap( args => {
                 args[0]['process.env']['VUE_APP_LEAGUE_NAME'] = JSON.stringify(process.env.VUE_APP_LEAGUE_NAME);
-                args[0]['process.env']['VUE_APP_API_URL'] = JSON.stringify(process.env.VUE_APP_LEAGUE_NAME);
+                args[0]['process.env']['VUE_APP_API_URL'] = JSON.stringify(process.env.VUE_APP_API_URL);
                 return args;
             })
     }
