2266973375ca3d349cf7b2bc1c6563ae7de3962f
TheTemptingSavior <TheTemptingSavior@protonmail.com>
2023-07-04 11:52:03 +0100

    Updated the 404 page to use Vuetify

--- frontend/src/views/errors/NotFound.vue
+++ frontend/src/views/errors/NotFound.vue
@@ -1,16 +1,15 @@
 <template>
-  <div class="section no-pad-bot" id="index-banner">
-    <div class="container">
-      <br><br>
-      <h1 class="header center orange-text">Page Not Found <span>:(</span></h1>
-      <br /><br />
-      <div class="row center">
-        <router-link to="/" class="btn-large waves-effect waves-light orange">
-          Take me home
-        </router-link>
-      </div>
-    </div>
-  </div>
+  <v-container fill-height fluid>
+    <v-row align="center" justify="center">
+      <v-col cols="12" xs="12" class="text-center">
+        <h2 class="accent--text text-h2">Page Not Found</h2>
+        <v-btn x-large shaped elevation="5" outlined to="/">
+          Take me back
+        </v-btn>
+      </v-col>
+    </v-row>
+  </v-container>
+
 </template>
 
 <script>
@@ -20,137 +19,5 @@ name: "NotFound"
 </script>
 
 <style scoped>
-::-moz-selection {
-  background: #b3d4fc;
-  text-shadow: none;
-}
 
-::selection {
-  background: #b3d4fc;
-  text-shadow: none;
-}
-
-html {
-  padding: 30px 10px;
-  font-size: 20px;
-  line-height: 1.4;
-  color: #737373;
-  background: #f0f0f0;
-  -webkit-text-size-adjust: 100%;
-  -ms-text-size-adjust: 100%;
-}
-
-html,
-input {
-  font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
-}
-
-body {
-  max-width: 500px;
-  _width: 500px;
-  padding: 30px 20px 50px;
-  border: 1px solid #b3b3b3;
-  border-radius: 4px;
-  margin: 0 auto;
-  box-shadow: 0 1px 10px #a7a7a7, inset 0 1px 0 #fff;
-  background: #fcfcfc;
-}
-
-h1 {
-  margin: 0 10px;
-  font-size: 50px;
-  text-align: center;
-}
-
-h1 span {
-  color: #bbb;
-}
-
-h3 {
-  margin: 1.5em 0 0.5em;
-}
-
-p {
-  margin: 1em 0;
-}
-
-ul {
-  padding: 0 0 0 40px;
-  margin: 1em 0;
-}
-
-.container {
-  max-width: 380px;
-  _width: 380px;
-  margin: 0 auto;
-}
-
-/* google search */
-
-#goog-fixurl ul {
-  list-style: none;
-  padding: 0;
-  margin: 0;
-}
-
-#goog-fixurl form {
-  margin: 0;
-}
-
-#goog-wm-qt,
-#goog-wm-sb {
-  border: 1px solid #bbb;
-  font-size: 16px;
-  line-height: normal;
-  vertical-align: top;
-  color: #444;
-  border-radius: 2px;
-}
-
-#goog-wm-qt {
-  width: 220px;
-  height: 20px;
-  padding: 5px;
-  margin: 5px 10px 0 0;
-  box-shadow: inset 0 1px 1px #ccc;
-}
-
-#goog-wm-sb {
-  display: inline-block;
-  height: 32px;
-  padding: 0 10px;
-  margin: 5px 0 0;
-  white-space: nowrap;
-  cursor: pointer;
-  background-color: #f5f5f5;
-  background-image: -webkit-linear-gradient(rgba(255,255,255,0), #f1f1f1);
-  background-image: -moz-linear-gradient(rgba(255,255,255,0), #f1f1f1);
-  background-image: -ms-linear-gradient(rgba(255,255,255,0), #f1f1f1);
-  background-image: -o-linear-gradient(rgba(255,255,255,0), #f1f1f1);
-  -webkit-appearance: none;
-  -moz-appearance: none;
-  appearance: none;
-  *overflow: visible;
-  *display: inline;
-  *zoom: 1;
-}
-
-#goog-wm-sb:hover,
-#goog-wm-sb:focus {
-  border-color: #aaa;
-  box-shadow: 0 1px 1px rgba(0, 0, 0, 0.1);
-  background-color: #f8f8f8;
-}
-
-#goog-wm-qt:hover,
-#goog-wm-qt:focus {
-  border-color: #105cb6;
-  outline: 0;
-  color: #222;
-}
-
-input::-moz-focus-inner {
-  padding: 0;
-  border: 0;
-}
 </style>
