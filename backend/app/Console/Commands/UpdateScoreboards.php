9de4fa4d45beaf3c99bfdb24b0e68977ca98afe2
TheTemptingSavior <TheTemptingSavior@protonmail.com>
2023-07-04 11:50:14 +0100

    Added artisan commands to run updates for scoreboards

---
+++ backend/app/Console/Commands/UpdateScoreboards.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Console\Commands;
+
+
+use App\Helpers\ScoreboardHelper;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
+
+class UpdateScoreboards extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'manual:update-scoreboards';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Updates the scoreboards for each season in the database';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $this->info('Beginning update of all league scoreboards.');
+
+        $seasons = DB::table('seasons')->select(['id', 'slug'])->get();
+        $this->info('Found ' . $seasons->count() . ' seasons to update.');
+        foreach($seasons as $s) {
+            $this->info('Starting update of season ' . $s->slug);
+            if (ScoreboardHelper::calculateScoreboard($s->id)) {
+                $this->info('Finished update of season ' . $s->slug);
+            } else {
+                $this->error('Failed update of season ' . $s->slug . '. Please check the logs.');
+                return 1;
+            }
+        }
+
+        return 0;
+    }
+}
