e96adee2bc094e866e19d08d98adba31e35e631c
TheTemptingSavior <TheTemptingSavior@protonmail.com>
2023-07-04 11:52:26 +0100

    added separate file for state

---
+++ frontend/src/store/state.js
@@ -0,0 +1,22 @@
+export default {
+    loading: false,
+    user: {
+        token: null,
+        username: null,
+        expiresIn: null,
+        receiveTime: null
+    },
+    current: null,
+    all: {},
+    seasons: [],
+    // ---
+    scoreboards: {
+        current: {},
+        all: {}
+    },
+    tracker: {
+        current: {},
+        all: {}
+    },
+    teams: [],
+}
