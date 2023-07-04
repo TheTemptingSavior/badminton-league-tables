245a23638926a38dc6675618f87e3b0dd0c879b0
TheTemptingSavior <TheTemptingSavior@protonmail.com>
2023-07-04 11:52:26 +0100

    Changed current to a key rather than copy of object

--- frontend/src/store/getters.js
+++ frontend/src/store/getters.js
@@ -1,8 +1,17 @@
+const getScoreboard = state => state.all[state.current].scoreboard;
+const getSeason = state => state.all[state.current].season;
+const getTeams = state => state.all[state.current].teams;
+const getTracker = state => state.all[state.current].tracker;
+
 const user = state => state.user;
 const token = state => state.user.token;
 const teams = state => state.teams;
 
 export default {
+    getScoreboard,
+    getSeason,
+    getTeams,
+    getTracker,
     user,
     token,
     teams
