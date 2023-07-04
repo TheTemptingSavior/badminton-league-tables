475d279a5057903966dcb78f6bd59fc83bbd72b7
TheTemptingSavior <TheTemptingSavior@protonmail.com>
2023-07-04 11:47:31 +0100

    Added swagger UI to the API

--- src/app/Models/User.php
+++ src/app/Models/User.php
@@ -10,9 +10,57 @@ use Illuminate\Database\Eloquent\Model;
 use Laravel\Lumen\Auth\Authorizable;
 use Tymon\JWTAuth\Contracts\JWTSubject;
 
+/**
+ * Class User
+ * @OA\Schema
+ * @package App\Models
+ */
 class User extends Model implements AuthenticatableContract, AuthorizableContract, JWTSubject
 {
     use Authenticatable, Authorizable;
+
+    /**
+     * @OA\Property()
+     * @var string
+     */
+    public $username;
+    /**
+     * @OA\Property(
+     *     type="string",
+     *     format="password"
+     * )
+     * @var string
+     */
+    public $password;
+    /**
+     * @OA\Property()
+     * @var boolean
+     */
+    public $admin;
+    /**
+     * @OA\Property(
+     *     type="string",
+     *     format="date-time"
+     * )
+     * @var string
+     */
+    public $added;
+    /**
+     * @OA\Property(
+     *     type="string",
+     *     format="date-time"
+     * )
+     * @var string
+     */
+    public $created_at;
+    /**
+     * @OA\Property(
+     *     type="string",
+     *     format="date-time"
+     * )
+     * @var string
+     */
+    public $updated_at;
 
     /**
      * The table associated with the model.
